e4d9bf1598d145d51d25c5b216b049de6389afab
Juan Gonon <juanfegonon27@gmail.com>
2024-12-27 10:31:04 -0600

    simple server

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import { WebSocketServer } from 'ws'
+
+const wss = new WebSocketServer({ port: 3000 })
+
+wss.on('connection', function connection (ws) {
+  console.log('Client connected')
+
+  ws.on('error', console.error)
+
+  ws.on('message', function message (data) {
+    console.log('received: %s', data)
+  })
+
+  ws.send('Hola desde el servidor!!')
+})
+
+console.log('Server running on port http://localhost:3000')
