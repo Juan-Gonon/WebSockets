107712bc0d66fcd3cb3e64c4e47e8dd7e4e7ac47
Juan Gonon <juanfegonon27@gmail.com>
2024-12-27 19:40:49 -0600

    message

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-base-to-string */
 import { WebSocketServer } from 'ws'
 
 const wss = new WebSocketServer({ port: 3000 })
@@ -8,14 +9,20 @@ wss.on('connection', function connection (ws) {
   ws.on('error', console.error)
 
   ws.on('message', function message (data) {
-    console.log('received: %s', data)
+    console.log('desde el cliente', data.toString())
+
+    const payload = {
+      type: 'custom-message',
+      payload: data.toString()
+    }
+    ws.send(JSON.stringify(payload))
   })
 
   ws.on('close', () => {
     console.log('client disconnected')
   })
 
-  ws.send('Hola desde el servidor!!')
+  // ws.send('Hola desde el servidor!!')
 })
 
 console.log('Server running on port http://localhost:3000')
